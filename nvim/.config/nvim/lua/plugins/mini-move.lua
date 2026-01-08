2ec52051f5bd6176ab117787a0f8b876845a581a
wytchblade <66.wytchblade@gmail.com>
2026-01-07 17:31:55 -0700

    |.git| = 6796

---
+++ nvim/.config/nvim/lua/plugins/mini-move.lua
@@ -0,0 +1,13 @@
+return {
+  'nvim-mini/mini.nvim', version = false
+  opts = {
+    -- add default mappings
+    mappings = true,
+    -- add default motions
+    motions = true,
+    -- add default operators
+    operators = true,
+    -- add default text objects
+    text_objects = true,
+  },
+}
